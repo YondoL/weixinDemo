b6ae27bf82b1c2868f543b54862a3db0d9a3043e
Yondol <1628916868@qq.com>
2017-04-11 14:43:04 +0800

    add big rice

--- js/common.js
+++ js/common.js
@@ -5,7 +5,14 @@
         recalc = function () {
         var clientWidth = docEl.clientWidth;
         if (!clientWidth) return;
-                docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+                console.log(clientWidth);
+                if(clientWidth >= 430){
+                        docEl.style.fontSize = 81.09375 + "px";
+                        $(".masktit").css("top","0.9rem");
+                }else{       
+                        docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+                }
+                console.log(docEl.style.fontSize);
         };
         if (!doc.addEventListener) return;
                 win.addEventListener(resizeEvt, recalc, false);
